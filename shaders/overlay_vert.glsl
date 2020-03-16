8429958980aad45673a5980a9ed4c1f338e92d51
Carl van Mastrigt <carl.van.mastrigt@gmail.com>
2020-03-16 13:07:42 +1000

    small fix in overlay_vert w component

--- shaders/overlay_vert.glsl
+++ shaders/overlay_vert.glsl
@@ -30,7 +30,7 @@ flat out ivec4 data3_;
 void main()
 {
     vec4 ps=vec4(data1);
-    gl_Position=vec4( ((ps.xy+ps.zw*vertex)*inv_window_size-0.5)*vec2(1.0,-1.0) ,0.0,0.5);
+    gl_Position=vec4( ((ps.xy+ps.zw*vertex)*inv_window_size-0.5)*vec2(2.0,-2.0) ,0.0,1.0);
 
     data2_=data2;
     data3_=data3;
