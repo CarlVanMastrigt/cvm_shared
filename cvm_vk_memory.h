956b17130bbb68c6160036c1c887f8b872fce083
Carl van Mastrigt <carl.van.mastrigt@gmail.com>
2021-07-03 13:54:55 +1000

    renamed and recycled some variables in new managed buffer struct

--- cvm_vk_memory.h
+++ cvm_vk_memory.h
@@ -47,19 +47,12 @@ typedef struct cvm_vk_dynamic_buffer_allocation cvm_vk_dynamic_buffer_allocation
 
 struct cvm_vk_dynamic_buffer_allocation/// (dont ref this by pointer, only store index)
 {
-    /// is possible to make all contents here bitfields (20->12 bytes), no noticeable drop in performance was observed when doing so, need to set CVM_VK_NULL_ALLOCATION_INDEX appropriately if doing so
-    /// does limit to 2M max allocations, but this should be more than enough
-    cvm_vk_dynamic_buffer_allocation * left;
-    cvm_vk_dynamic_buffer_allocation * right;
-
-    union
-    {
-        cvm_vk_dynamic_buffer_allocation * next;
-        uint32_t heap_index;
-    }link;
+    cvm_vk_dynamic_buffer_allocation * prev;
+    cvm_vk_dynamic_buffer_allocation * next;/// both next in horizontal structure of all buffers and next in linked list of unused allocations
 
     uint32_t offset;/// actual offset = base_size<<offset_factor base allocation size 10 allows 16G buffer (which i quite like)
     ///get bit for "is_right_buffer" by 1 & (offset >> size_factor) during recombination calculations (256M max buffer size and 256 byte min gives the required size of this)
+    uint32_t heap_index:25;///32m should be enough...
     uint32_t size_factor:5;
     uint32_t available:1;///does not have contents, can be retrieved by new allocation or recombined into
     uint32_t is_base:1;///need to free this pointer at end
@@ -91,21 +84,16 @@ typedef struct cvm_vk_managed_buffer
     cvm_vk_dynamic_buffer_allocation * first_unused_allocation;///singly linked list of allocations to assign space to
     uint32_t unused_allocation_count;
 
-    cvm_vk_dynamic_buffer_allocation * rightmost_allocation;///used for setting left/right of new allocations
+    cvm_vk_dynamic_buffer_allocation * last_used_allocation;///used for setting left/right of new allocations
 
     ///consider making following fixed array with max size of ~24 (whole struct should fit on 2-3 cache lines)
     cvm_vk_availablility_heap * available_dynamic_allocations;
     ///intelligent application of last pointer to linked list creation isn't applicable, cannot reference/dereference bitfield!
     uint32_t num_dynamic_allocation_sizes;///essentially max value of dynamic_buffer_allocation.size_factor derived from the exponent of max_allocation_size/base_allocation_size
     uint32_t available_dynamic_allocation_bitmask;
-    ///defragger would also need to keep track of active allocations
-    //potentially worth it to sort by index (especially if defragging) then "relinquish" last dynamic_buffer_allocation and decrease count
-    // ^ can be done recursively by checking if left dynamic_buffer_allocation has been relinquished, then removing it from its linked list
-    //     ^ because defragging of these allocations requires copying contents of one dynamic_buffer_allocation to another this wont work very well at all!
     uint32_t base_dynamic_allocation_size_factor;
 
-    void * mapping;///for use on UMA platforms
-
+    void * mapping;///used for device generic buffers on UMA platforms and staging/uniform buffers on all others
 }
 cvm_vk_managed_buffer;
 
@@ -142,51 +130,51 @@ uint64_t cvm_vk_acquire_static_buffer_allocation(cvm_vk_managed_buffer * mb,uint
 
 ///can reasonably easily combine the node and the allocation list
 
-typedef struct cvm_vk_dynamic_buffer_allocation_3 cvm_vk_dynamic_buffer_allocation_3;
-
-/// *just* fits in one cache line
-struct cvm_vk_dynamic_buffer_allocation_3/// (dont ref this by pointer, only store index)
-{
-    ///horizontal structure
-    cvm_vk_dynamic_buffer_allocation_3 * left;
-    cvm_vk_dynamic_buffer_allocation_3 * right;
-
-    ///
-    cvm_vk_dynamic_buffer_allocation_3 * l_child;
-    cvm_vk_dynamic_buffer_allocation_3 * r_child;
-    cvm_vk_dynamic_buffer_allocation_3 * parent;///also used in singly linked list
-
-    uint64_t offset;
-    uint32_t size;
-    int8_t bias;
-    uint8_t free;///if copying contents of 2 allocations as part of defragger then DO NOT copy this variable
-};
-
-///no distinction between static and dynamic allocations
-typedef struct cvm_vk_managed_buffer_3
-{
-    VkBuffer buffer;
-    VkDeviceMemory memory;
-
-    void * mapping;///for use on UMA platforms
-
-    uint64_t total_space;
-    uint64_t used_space;
-    uint64_t base_alignment;
-
-    cvm_vk_dynamic_buffer_allocation_3 * first_unused_allocation;///singly linked list of allocations to assign space to, will only ever require a single new allocation at a time
-    cvm_vk_dynamic_buffer_allocation_3 * rightmost_allocation;///used for setting left/right of new allocations
-
-    uint32_t reserved_allocation_count;/// max allocations expected to be made per frame (amount to expand by when reallocing?)
-    uint32_t unused_allocation_count;
-}
-cvm_vk_managed_buffer_3;
-
-void cvm_vk_create_managed_buffer_3(cvm_vk_managed_buffer_3 * buffer,uint64_t buffer_size,uint32_t reserved_allocation_count,VkBufferUsageFlags usage);///determine alignment from usage flags and round size up to multiple
-void cvm_vk_destroy_managed_buffer_3(cvm_vk_managed_buffer_3 * mb);
-
-uint32_t cvm_vk_acquire_dynamic_buffer_allocation_3(cvm_vk_managed_buffer_3 * mb,uint64_t size);
-void cvm_vk_relinquish_dynamic_buffer_allocation_3(cvm_vk_managed_buffer_3 * mb,uint32_t index);
+//typedef struct cvm_vk_dynamic_buffer_allocation_3 cvm_vk_dynamic_buffer_allocation_3;
+//
+///// *just* fits in one cache line
+//struct cvm_vk_dynamic_buffer_allocation_3/// (dont ref this by pointer, only store index)
+//{
+//    ///horizontal structure
+//    cvm_vk_dynamic_buffer_allocation_3 * left;
+//    cvm_vk_dynamic_buffer_allocation_3 * right;
+//
+//    ///
+//    cvm_vk_dynamic_buffer_allocation_3 * l_child;
+//    cvm_vk_dynamic_buffer_allocation_3 * r_child;
+//    cvm_vk_dynamic_buffer_allocation_3 * parent;///also used in singly linked list
+//
+//    uint64_t offset;
+//    uint32_t size;
+//    int8_t bias;
+//    uint8_t free;///if copying contents of 2 allocations as part of defragger then DO NOT copy this variable
+//};
+//
+/////no distinction between static and dynamic allocations
+//typedef struct cvm_vk_managed_buffer_3
+//{
+//    VkBuffer buffer;
+//    VkDeviceMemory memory;
+//
+//    void * mapping;///for use on UMA platforms
+//
+//    uint64_t total_space;
+//    uint64_t used_space;
+//    uint64_t base_alignment;
+//
+//    cvm_vk_dynamic_buffer_allocation_3 * first_unused_allocation;///singly linked list of allocations to assign space to, will only ever require a single new allocation at a time
+//    cvm_vk_dynamic_buffer_allocation_3 * rightmost_allocation;///used for setting left/right of new allocations
+//
+//    uint32_t reserved_allocation_count;/// max allocations expected to be made per frame (amount to expand by when reallocing?)
+//    uint32_t unused_allocation_count;
+//}
+//cvm_vk_managed_buffer_3;
+//
+//void cvm_vk_create_managed_buffer_3(cvm_vk_managed_buffer_3 * buffer,uint64_t buffer_size,uint32_t reserved_allocation_count,VkBufferUsageFlags usage);///determine alignment from usage flags and round size up to multiple
+//void cvm_vk_destroy_managed_buffer_3(cvm_vk_managed_buffer_3 * mb);
+//
+//uint32_t cvm_vk_acquire_dynamic_buffer_allocation_3(cvm_vk_managed_buffer_3 * mb,uint64_t size);
+//void cvm_vk_relinquish_dynamic_buffer_allocation_3(cvm_vk_managed_buffer_3 * mb,uint32_t index);
 
 
 
