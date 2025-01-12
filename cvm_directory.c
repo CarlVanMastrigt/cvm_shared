85b5d2ba9e519c62575b05e5e785f3562d1290f3
CarlVanMastrigt <carl.van.mastrigt@gmail.com>
2025-01-13 02:13:46 +1100

    directory work

--- cvm_directory.c
+++ cvm_directory.c
@@ -17,6 +17,9 @@ You should have received a copy of the GNU Affero General Public License
 along with cvm_shared.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+#define _XOPEN_SOURCE 700
+
+#include <limits.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -27,71 +30,27 @@ along with cvm_shared.  If not, see <https://www.gnu.org/licenses/>.
 #include <sys/stat.h>
 #include <dirent.h>
 
-// all the tools to access a directory, should be created ONCE based on operating system
-struct cvm_directory_utils
-{
-	uint32_t acquire_count;// basically reference count, used to ensure it gets cleaned up properly
-
-
-	// in byres (for UTF-8), 0 for unlimited, should include extension and null terminator
-	uint32_t max_path_length;
-	uint32_t max_entry_length;
-	// fallback_directory is guaranteed to exist
-	const char* fallback_directory;
-
-	// if query is true, will not attempt to change directory, just query if its possible
-	bool (*const up_dir)   (struct cvm_directory*, uint32_t count, bool query);
-
-	bool (*const make_dir) (struct cvm_directory*, char* dir_name);
-
-	// not sure this one should exist...
-	bool (*const set_dir)  (struct cvm_directory*, char* full_path);
-
-	// entry index must correspond to a directory
-	bool (*const enter_index) (struct cvm_directory*, uint32_t entry_index);
-
-	// entry index must correspond to a directory
-	bool (*const enter_name) (struct cvm_directory*, uint32_t entry_index);
-
-	/**
-	 * can be used to acquire a file to fopen
-	 * if `path` is NULL, will return the required size of path in `path_bytes`, otherwise `path` MUST have `*path_bytes` bytes of space, which must have been returned by a prior call
-	 * */
-	bool (*const get_entry_full_path) (struct cvm_directory*, uint32_t entry_index, char* path, uint32_t* path_bytes);
-
-	/**
-	 * can be used to acquire a file to fopen
-	 * if `path` is NULL, will return the required size of path in `path_bytes`, otherwise `path` MUST have `*path_bytes` bytes of space, which must have been returned by a prior call
-	 * */
-	bool (*const get_file_full_path)  (struct cvm_directory*, const char* filename, char* path, uint32_t* path_bytes);
-};
-
-/// have these per platform ??
-struct cvm_directory_utils* cvm_directory_utils_acquire(void);
-struct cvm_directory_utils* cvm_directory_utils_release(void);
-
-
-#warning instead of above could employ a base set of functions that get used
 
 
 
+#include <errno.h>
 
 
 
-
-static inline void add_entry(struct cvm_directory* directory, char* name, bool is_dir, uint64_t mod_t_sec, uint32_t mod_t_nsec)
+static inline void cvm_directory_add_entry(struct cvm_directory* directory, char* name, uint32_t flags, uint64_t mod_t_sec, uint32_t mod_t_nsec)
 {
 	size_t len = strlen(name) + 1;//+1 for /0 terminator character
+	assert(mod_t_nsec < 1000000000);
 
-	if(directory->entry_count == directory->entry_space)
+	if(directory->entry_total_count == directory->entry_space)
 	{
 		directory->entry_space *= 2;
 		directory->entries = realloc(directory->entries, sizeof(struct cvm_directory_entry) * directory->entry_space);
 	}
-	directory->entries[directory->entry_count++] = (struct cvm_directory_entry)
+	directory->entries[directory->entry_total_count++] = (struct cvm_directory_entry)
 	{
 		.entry_name_offset = directory->filename_buffer_offset,
-		.is_dir = is_dir,
+		.flags = flags,
 		.mod_t_nsec = mod_t_nsec,
 		.mod_t_sec = mod_t_sec,
 	};
@@ -106,11 +65,8 @@ static inline void add_entry(struct cvm_directory* directory, char* name, bool i
 	directory->filename_buffer_offset += len;
 }
 
-
-
-
 ///file directory should map indices (0-n in display) to relevant lookup information (type and index in dir) 
-static void ensure_path_space(struct cvm_directory* directory, size_t required_space)
+static inline void cvm_directory_ensure_path_space(struct cvm_directory* directory, size_t required_space)
 {
 	if(required_space > directory->path_space)
     {
@@ -121,97 +77,168 @@ static void ensure_path_space(struct cvm_directory* directory, size_t required_s
 }
 
 
+// static inline int cvm_directory_sort_function(const void* a, const void* b, void* context)
+// {
+// 	const struct cvm_directory* directory = context;
+// 	const struct cvm_directory_entry* entry_1 = a;
+// 	const struct cvm_directory_entry* entry_2 = b;
 
+// 	#warning need to apply filter here as that overrides sort
+// 	/// ALSO can track count for filtering!
 
+// 	if(directory->sort_function)
+// 	{
+// 		return directory->sort_function(directory, entry_1, entry_2);
+// 	}
+// 	else
+// 	{
+// 		return 0;
+// 	}
+// }
 
-void cvm_clean_path_dirent(struct cvm_directory* directory, bool append_dir_marker)
+static inline void cvm_directory_sort(struct cvm_directory* directory)
 {
-	const char* read;
-	char* write;
+	// qsort_s(directory->entries, directory->entry_total_count, sizeof(struct cvm_directory_entry), &cvm_directory_sort_function, directory);
+	//cannot use qsort() easily as entries need to access external data, instead need to sort with own function...
+}
+
+
+
+
+
+
+bool cvm_directory_util_set_path(struct cvm_directory* directory, const char* path);
+void cvm_directory_util_scan(struct cvm_directory* directory);
+void cvm_directory_util_goto_parent(struct cvm_directory* directory);
+void cvm_directory_util_get_path(struct cvm_directory* directory, const char* to_append, size_t* required_space, char* path_buffer);
+
+
+
+/// this function sets length so it must not use extant length
+bool cvm_directory_util_set_path(struct cvm_directory* directory, const char* path)
+{
+	char* new_path;
 	size_t len;
 
-	if(append_dir_marker)
+	#warning need to handle errors (in a better way), specifically a missing folder path, which should be resolvable/constructable? -- create with force
+	
+	new_path = realpath(path, NULL);
+	if(new_path)
 	{
-		len = strlen(directory->path);
-		ensure_path_space(directory, len + 2);
-		if(len && (directory->path[len-1] != '/'))
+		len = strlen(new_path);
+		cvm_directory_ensure_path_space(directory, len + 2);
+		strcpy(directory->path, new_path);
+		if((len>0) && (directory->path[len-1] != '/'))
 		{
-			directory->path[len] = '/';
-			directory->path[len+1] = '\0';
+			directory->path[len++] = '/';
+			directory->path[len] = '\0';
 		}
-		else
-		{
-			directory->path[0] = '/';
-			directory->path[1] = '\0';
-		}
+		free(new_path);
+		directory->path_length = len;
+		return true;
 	}
-
-	read = directory->path;
-	write = directory->path;
-
-    while(*read)
-    {
-        while(*read=='/')
-        {
-            if((read[1] == '.') && (read[2] == '.') && (read[3] == '/'))// go up a layer when encountering ../
-            {
-                read += 3;// skip read over ../
-                // for both: ensure we don't skip over root /
-                if(write > directory->path) write--;// reverse over most recent '/'
-                while((write > directory->path) && (*write != '/')) write--; // move write back one dir marker if possible
-            }
-            else if((read[1]=='.') && (read[2]=='/')) read += 2;///remove ./
-            else if(read[1]=='/') read++;// remove multiple /
-            else break;
-        }
-
-        if(write != read) *write = *read;// avoid reading and writing same char
-
-        read++;
-        write++;
-    }
-
-    *write = '\0';
-
-    directory->path_length = write - directory->path;
+	return false;
 }
 
 #warning should probably only support regular files and directories unless requested otherwise
-/// bitmask of options??
-static inline bool cvm_scan_dir_dirent(struct cvm_directory* directory, const char* path)
+/// bitmask of options?? -- options could include listing .. and . "LIST_CONVENIENCE_ENTRIES"
+void cvm_directory_util_scan(struct cvm_directory* directory)
 {
 	DIR * d;
     struct dirent* e;
     struct stat s;
     size_t len;
+    uint32_t flags;
 
-    len = strlen(path);
-    ensure_path_space(directory, len + 2);// need space to pas with / and \0
-    memcpy(directory->path, path, sizeof(char) * (len + 1));// copy with \0
-    cvm_clean_path_dirent(directory, true);// also sets length
-
-    directory->entry_count = 0;
+    directory->entry_total_count = 0;
+    directory->entry_filtered_count = 0;
     directory->filename_buffer_offset = 0;
 
-    d = opendir(path);
+    d = opendir(directory->path);
 
     if(!d)
     {
-    	return false;
+    	// this shouldn't happen, should fix this issue when path is set (cvm_directory_util_set_path)
+    	fprintf(stderr,"dir to open invalid (%s)\n", directory->path);
+    	return;
     }
 
     while((e=readdir(d)))
     {
     	len = strlen(e->d_name);
-    	ensure_path_space(directory, directory->path_length + len + 1);
+    	// use directory->path to stash the filepath being assessed
+    	cvm_directory_ensure_path_space(directory, directory->path_length + len + 1);
     	memcpy(directory->path + directory->path_length, e->d_name, sizeof(char) * (len + 1));
     	stat(directory->path, &s);
-    	add_entry(directory, e->d_name, S_ISDIR(s.st_mode), s.st_mtime, 0);
+
+    	assert(__builtin_popcount(s.st_mode & S_IFMT) <= 1);// only 1 should be set
+    	switch (s.st_mode & S_IFMT) {
+    		case S_IFBLK:	flags = CVM_DIRECTORY_FLAG_BLOCK_DEV;    break;
+			case S_IFCHR:	flags = CVM_DIRECTORY_FLAG_CHAR_DEV;     break;
+			case S_IFDIR:	flags = CVM_DIRECTORY_FLAG_DIRECTORY;    break;
+			case S_IFIFO:	flags = CVM_DIRECTORY_FLAG_PIPE;         break;
+			case S_IFLNK:	flags = CVM_DIRECTORY_FLAG_LINK;         break;// can a link also be a directory?? if so we should handle that
+			case S_IFREG:	flags = CVM_DIRECTORY_FLAG_REGULAR_FILE; break;
+			case S_IFSOCK:	flags = CVM_DIRECTORY_FLAG_SOCKET;       break;
+			default:        flags = CVM_DIRECTORY_FLAG_UNKNOWN;      break;
+    	}
+
+    	// are convenience files implicitly hidden?
+    	if( !strcmp(e->d_name, ".") || !strcmp(e->d_name, ".") ) flags |= CVM_DIRECTORY_FLAG_CONVENIENCE;
+    	if( len && e->d_name[0] == '.') flags |= CVM_DIRECTORY_FLAG_HIDDEN;
+
+    	printf("%s : %s : %lu : %u\n", S_ISDIR(s.st_mode)?"folder":(S_ISREG(s.st_mode)?"file":"?"), directory->path, s.st_mtim.tv_sec, s.st_mtim.tv_nsec);
+    	cvm_directory_add_entry(directory, e->d_name, flags, s.st_mtim.tv_sec, s.st_mtim.tv_nsec);
     	directory->path[directory->path_length] = '\0';
     }
-    // directory->path[directory->path_length] = '\0';
-
 }
+
+void cvm_directory_util_goto_parent(struct cvm_directory* directory)
+{
+	bool parent_exists;
+	// lil bit hacky
+	cvm_directory_ensure_path_space(directory, directory->path_length + 3);
+	strcpy(directory->path + directory->path_length, "..");
+	parent_exists = cvm_directory_util_set_path(directory, directory->path);
+	assert(parent_exists);
+}
+
+void cvm_directory_util_get_path(struct cvm_directory* directory, const char* to_append, size_t* required_space, char* path_buffer)
+{
+	size_t append_len;
+	struct stat s;
+	int err;
+
+	// when making space, add 2, one of '\0' and one ofr a directory marking '/' in case the entry accessed is a directory
+	append_len = to_append ? strlen(to_append) : 0;
+
+	if(path_buffer)
+	{
+		assert(*required_space >= directory->path_length + append_len + 2);
+		strcpy(path_buffer, directory->path);
+		if(to_append)
+		{
+			strcpy(path_buffer + directory->path_length, to_append);
+			// for consistency, if accessed entry is a directory append a '/'
+			err = stat(path_buffer, &s);
+			if(!err && S_ISDIR(s.st_mode) && (path_buffer[directory->path_length + append_len - 1] != '/'))
+			{
+				strcpy(path_buffer + directory->path_length + append_len , "/");
+			}
+		}
+	}
+	else
+	{
+		*required_space = directory->path_length + append_len + 2;
+	}
+}
+
+
+
+
+
+
+
 
 
 
@@ -227,19 +254,80 @@ void cvm_directory_initialise(struct cvm_directory* directory, const char* initi
 
 		.entries = malloc(sizeof(struct cvm_directory_entry)*16),
 		.entry_space = 16,
-		.entry_count = 0,
+		.entry_total_count = 0,
+		.entry_filtered_count = 0,
+
+		.filter_exclude_flags = 0,/// set this!
+		.filter_extension_buffer = malloc(sizeof(char)*64),
+		.filter_extension_buffer_space = 64,
 
 		.filename_buffer = malloc(sizeof(char)*4096),
 		.filename_buffer_space = 4096,
 		.filename_buffer_offset = 0,
+
+		.sort_function = NULL,// set this!
 	};
 
-	cvm_scan_dir_dirent(directory, initial_path);
-
-
+	cvm_directory_util_set_path(directory, initial_path);
+	cvm_directory_util_scan(directory);
 }
 
 void cvm_directory_terminate(struct cvm_directory* directory)
 {
 	free(directory->fallback_path);
+	free(directory->path);
+	free(directory->entries);
+	free(directory->filter_extension_buffer);
+	free(directory->filename_buffer);
 }
+
+void cvm_directory_goto_parent(struct cvm_directory* directory)
+{
+	cvm_directory_util_goto_parent(directory);
+	cvm_directory_util_scan(directory);
+}
+
+void cvm_directory_get_entry_path(struct cvm_directory* directory, uint32_t entry_index, size_t* required_space, char* path_buffer)
+{
+	char* entry_name;
+
+	assert(entry_index < directory->entry_filtered_count);
+
+	entry_name = directory->filename_buffer + directory->entries[entry_index].entry_name_offset;
+
+	cvm_directory_util_get_path(directory, entry_name, required_space, path_buffer);
+}
+
+void cvm_directory_get_file_path(struct cvm_directory* directory, const char* path_relative_filename, size_t* required_space, char* path_buffer)
+{
+	cvm_directory_util_get_path(directory, path_relative_filename, required_space, path_buffer);
+}
+
+void cvm_directory_reload(struct cvm_directory* directory)
+{
+	///
+}
+
+void cvm_directory_set_ordering_name(struct cvm_directory* directory, bool reversed)
+{
+	//
+}
+
+void cvm_directory_set_ordering_time(struct cvm_directory* directory, bool reversed)
+{
+	//
+}
+
+void cvm_directory_set_ordering_custom(struct cvm_directory* directory, int (*sort_function)(const struct cvm_directory*, const struct cvm_directory_entry*, const struct cvm_directory_entry*))
+{
+	if(sort_function != directory->sort_function)
+	{
+		directory->sort_function = sort_function;
+		cvm_directory_sort(directory);
+	}
+}
+
+
+// for widgets that reference this we should avoid having anything built on top of it, this becomes complicated in the case of text bar though (for selections)
+// this can be done in custom fashion though (validating contents and querying basis before invalidating local contents)
+// it DOES however mean we should move all sort/filter operations to the directory
--- cvm_directory.h
+++ cvm_directory.h
@@ -26,12 +26,24 @@ along with cvm_shared.  If not, see <https://www.gnu.org/licenses/>.
 
 /// cannot and will not do path length validation as no guarantees exist cross platform
 
+// no flag for being a regular file
+#define CVM_DIRECTORY_FLAG_REGULAR_FILE	0x00000001
+#define CVM_DIRECTORY_FLAG_DIRECTORY	0x00000002
+// convenience listings are utilities like .. (parent) and . (current)
+#define CVM_DIRECTORY_FLAG_CONVENIENCE	0x00000004
+#define CVM_DIRECTORY_FLAG_HIDDEN		0x00000008
+#define CVM_DIRECTORY_FLAG_LINK			0x00000010
+#define CVM_DIRECTORY_FLAG_BLOCK_DEV	0x00000020
+#define CVM_DIRECTORY_FLAG_CHAR_DEV		0x00000040
+#define CVM_DIRECTORY_FLAG_PIPE			0x00000080
+#define CVM_DIRECTORY_FLAG_SOCKET		0x00000100
+#define CVM_DIRECTORY_FLAG_UNKNOWN		0x00000200
+
 struct cvm_directory_entry
 {
-	uint32_t entry_name_offset;
+	size_t entry_name_offset;
 	// bool is_directory;// subtype value?
-	uint32_t _reserved:1;
-	uint32_t is_dir:1;
+	uint32_t flags;
 	uint32_t mod_t_nsec:30;
 	uint64_t mod_t_sec;
 };
@@ -44,19 +56,41 @@ struct cvm_directory
 	size_t path_length;
 	size_t path_space;
 
-
-
 	struct cvm_directory_entry* entries;
-	size_t entry_count;
 	size_t entry_space;
+	size_t entry_total_count;
+	size_t entry_filtered_count;
 
-	char * filename_buffer;///shared buffer for all entry names, allows a good deal of compaction
+	uint32_t filter_exclude_flags;
+    /// composite buffer to iterate, with double null terminating character ['\0','\0'] at the end to know when to finish
+    char* filter_extension_buffer;
+    size_t filter_extension_buffer_space;
+
+	char* filename_buffer;///shared buffer for all entry names, allows a good deal of compaction
     size_t filename_buffer_space;
     size_t filename_buffer_offset;
+
+    int (*sort_function)(const struct cvm_directory*, const struct cvm_directory_entry*, const struct cvm_directory_entry*)/// if null_ptr take as-is (don't sort)
 };
+
+static inline const char* cvm_directory_get_entry_name(const struct cvm_directory* directory, const struct cvm_directory_entry* entry)
+{
+	return directory->filename_buffer + entry->entry_name_offset;
+}
+
+// to avoid reloading entries, flagged out ones could be put after the end of some valid_count
 
 // if `initial_path` is NULL will use `utils->fallback_directory`
 void cvm_directory_initialise(struct cvm_directory* directory, const char* initial_path, const char* fallback_path);
 void cvm_directory_terminate(struct cvm_directory* directory);
+
+
+void cvm_directory_goto_parent(struct cvm_directory* directory);
+void cvm_directory_get_entry_path(struct cvm_directory* directory, uint32_t entry_index, size_t* required_space, char* path_buffer);
+void cvm_directory_get_file_path(struct cvm_directory* directory, const char* path_relative_filename, size_t* required_space, char* path_buffer);
+void cvm_directory_reload(struct cvm_directory* directory);
+void cvm_directory_set_ordering_name(struct cvm_directory* directory, bool reversed);
+void cvm_directory_set_ordering_time(struct cvm_directory* directory, bool reversed);
+void cvm_directory_set_ordering_custom(struct cvm_directory* directory, int (*sort_function)(const struct cvm_directory*, const struct cvm_directory_entry*, const struct cvm_directory_entry*));
 
 #endif // CVM_DIRECTORY_H
